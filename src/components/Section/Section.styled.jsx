7b821a8ac0058ab1e85aa1ff42377ec2dc0836ca
Loki99911 <nikita_kresik@i.ua>
2022-12-08 23:05:36 +0200

    4

--- src/components/Section/Section.styled.jsx
+++ src/components/Section/Section.styled.jsx
@@ -1,8 +1,12 @@
 import styled from 'styled-components';
 
 export const SectionBlock = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
 width:50vw;
 margin:0 auto;
+margin-top:30px;
 `;
 
 export const SectionTitle = styled.h2`
